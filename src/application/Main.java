a1be0371ebbbc53e7ac682479a724d57ecb01619
vinnievanilla <vinnie.vanilla@gmail.com>
2018-04-19 12:12:35 +0200

    pierwszy commit lokalnie

---
+++ src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+
+public class Main extends Application {
+
+	public static Stage primaryStage;
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent parent = (Parent) FXMLLoader.load(getClass().getResource("/view/MainView.fxml"));
+			Scene scene = new Scene(parent);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			Main.primaryStage = primaryStage;
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/controllers/mainController.java
@@ -0,0 +1,73 @@
+package controllers;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import application.Main;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+
+
+public class mainController implements Initializable{
+
+    @FXML
+    private Button newGameButton;
+
+    @FXML
+    private Button loadGameButton;
+
+    @FXML
+    private ImageView enterAp1;
+
+    @FXML
+    private ImageView enterAp2;
+
+    @FXML
+    private ImageView enterAp3;
+
+    @FXML
+    private ImageView enterAp4;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		//open window 1
+		enterAp1.setOnMouseClicked((Event event)->openNewWindow("/view/Application1Wiew.fxml", ((Node)(event.getSource())).getScene().getWidth(), ((Node)(event.getSource())).getScene().getHeight()));
+
+		//open window 2
+		enterAp2.setOnMouseClicked((Event event)->openNewWindow("/view/Application2Wiew.fxml", ((Node)(event.getSource())).getScene().getWidth(), ((Node)(event.getSource())).getScene().getHeight()));
+
+		//open window 3
+		enterAp3.setOnMouseClicked((Event event)->openNewWindow("/view/Application3Wiew.fxml", ((Node)(event.getSource())).getScene().getWidth(), ((Node)(event.getSource())).getScene().getHeight()));
+
+		//open window 4
+		enterAp4.setOnMouseClicked((Event event)->openNewWindow("/view/Application4Wiew.fxml", ((Node)(event.getSource())).getScene().getWidth(), ((Node)(event.getSource())).getScene().getHeight()));
+
+	}
+
+    private void openNewWindow(String pathToFxmlFile, double width, double height){
+    	try {
+			Parent parent = (Parent) FXMLLoader.load(getClass().getResource(pathToFxmlFile));
+			Stage stage = new Stage();
+			stage.setScene(new Scene(parent, width, height));
+			stage.setOnShowing(ev -> stage.hide());
+			stage.setOnShown(ev -> {
+				stage.setX(( Main.primaryStage.getX() + Main.primaryStage.getWidth() / 2d) - stage.getWidth() / 2d);
+				stage.setY((Main.primaryStage.getY() + Main.primaryStage.getHeight() / 2d) - stage.getHeight() / 2d);
+				stage.show();
+			});
+			stage.showAndWait();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+    }
+}
